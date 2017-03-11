b1f0ab41e20bf229483236406fd9c16dce4a5fe3
wyn <wynaiyyf@126.com>
2017-03-11 16:49:16 +0800

    recopy

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
+
+
+def my_login(request):
+    if request.method == 'POST':
+        username = request.POST.get("username", "")
+        password = request.POST.get('password', "")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            data = {
+
+            }
+            return render(request, "index.html", data)
+    elif request.method == 'GET':
+        data = {
+
+        }
+        return render(request, "login.html", data)
--- muxue/urls.py
+++ muxue/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.views.generic import TemplateView
+from users.views import my_login
 
 import xadmin
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
-    url(r'login/', TemplateView.as_view(template_name='login.html'), name='login'),
+    url(r'login/$', my_login, name='login'),
 ]
